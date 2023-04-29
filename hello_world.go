e7384812aadacabda9380d1815888834a724b28e
IBM Terraform <shankar.pentyala@ibm.com>
2023-04-29 10:03:22 -0700

    hello world

---
+++ hello_world.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+/*
+The first line package main specifies that this is a standalone executable program rather than a library.
+The second line import "fmt" imports the fmt package, which provides formatting and printing functions.
+The func main() is the entry point of the program.
+The fmt.Println("Hello, World!") statement prints the message "Hello, World!" to the console.
+
+*/
