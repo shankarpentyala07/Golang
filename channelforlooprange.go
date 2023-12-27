da17dc1c36e72a04d313b9f325c6316cdc7b3770
shankar pentyala <shankarpentyala@gmail.com>
2023-12-27 14:59:18 -0800

    Create channelforlooprange.go

---
+++ channelforlooprange.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func producer(chn1 chan int) {
+	for i := 0; i < 10; i++ {
+		chn1 <- i
+	}
+	close(chn1)
+}
+func main() {
+	ch := make(chan int)
+	go producer(ch)
+	for v := range ch {
+		fmt.Println("Received ", v)
+	}
+}
