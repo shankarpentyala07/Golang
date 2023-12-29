96746e028d06d108ea96e9713d3abfbc727cda32
shankar pentyala <shankarpentyala@gmail.com>
2023-12-29 19:53:06 +0000

    packages

--- Golang/day-02/go.mod
+++ Golang/day-02/go.mod
@@ -1,3 +1,5 @@
 module day2
 
 go 1.21.5
+
+require github.com/google/uuid v1.5.0
--- Golang/day-02/main.go
+++ Golang/day-02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"day2/shapes"
 	"fmt"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -11,4 +12,10 @@ func main() {
 
 	s := shapes.Square{5}
 	fmt.Println("Area of Square:", s.AreaOfSquare())
+
+	fmt.Println(uuid.NewUUID())
+
+
+
+
 }
