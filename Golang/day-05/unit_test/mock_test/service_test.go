c63a5f7438f76f467adf03171aba7031a10abbe2
shankarpentyala07 <shankarpentyala@gmail.com>
2024-01-20 17:58:59 -0800

    testing profiling benchmarking

---
+++ Golang/day-05/unit_test/mock_test/service_test.go
@@ -0,0 +1,29 @@
+package myservice
+
+import (
+	"day5/unit_test/mock_test/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDoSomething(t *testing.T) {
+
+	//create an instance of our test object
+	apiMock := new(mocks.ExternalAPIMock)
+
+	//Setup expectations
+	apiMock.On("FetchData", "foo").Return("bar", nil)
+
+	//Create an instance of MyService using the mock
+	myService := New(apiMock)
+
+	// Call the method we want to test
+	result, err := myService.DoSomething("foo")
+
+	// Assert expectations
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", result)
+	apiMock.AssertExpectations(t)
+
+}
